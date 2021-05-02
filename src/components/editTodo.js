c1b74aaa589bddd0265dbe44a009da14b3f71832
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-05-02 18:14:43 +0500

    edit

--- src/components/editTodo.js
+++ src/components/editTodo.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
-export const editTodo = ({title, setTitle}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import React from "react";
+ 
+const editTodo = (props) => {
+  return (
+    <div className="popup-box">
+      <div className="box">
+                            content={<>
+                                <b>Design your Popup</b>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <button>Test button</button>
+                            </>}
+                            handleClose={togglePopup}
+      </div>
+    </div>
+  );
+};
+ 
+export default editTodo;
